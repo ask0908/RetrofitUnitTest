ef0e22f0ab9dcbfc13af535bde22b42393e4cd7d
jit0908 <ne0001913@gmail.com>
2022-01-17 00:37:22 +0900

    주석 수정

--- app/src/main/java/com/example/retrofitunittest/AuthInterceptor.java
+++ app/src/main/java/com/example/retrofitunittest/AuthInterceptor.java
@@ -8,8 +8,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 /**
- * This class add information (API Key) to {@link okhttp3.OkHttpClient} which is passed in
- * {@link TmdbApiClient#getRestAdapter()} which is required when making a request.
+ * This class add information (API Key) to {@link okhttp3.OkHttpClient} which is passed in {@link TmdbApiClient#getRestAdapter()} which is required when making a request.
+ * 이 클래스는 요청 시 필요한 정보(API Key)를 {@link TmdbApiClient#getRestAdapter()}로 전달되는 {@link okhttp3.OkHttpClient}에 추가합니다.
  *
  * @author Thomas Kioko
  */
@@ -25,7 +25,7 @@ public class AuthInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         HttpUrl url = request.url().newBuilder()
-                .addQueryParameter("api_key", "88b7a3f84f260070a5ab73f2af65ffe1")
+                .addQueryParameter("api_key", "88b7a3f84f260070a5ab73f2af65ffe1")   // API key를 안전하게 보관하는 방법?
                 .build();
         request = request.newBuilder().url(url).build();
         return chain.proceed(request);
