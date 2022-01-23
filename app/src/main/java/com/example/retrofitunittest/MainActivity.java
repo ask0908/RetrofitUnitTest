5824cdef1de1e8a5fda9c9ee2663c6962a9eb7c5
jit0908 <ne0001913@gmail.com>
2022-01-24 00:51:45 +0900

    MockWebserver를 사용한 레트로핏 유닛 테스트 예시 추가

--- app/src/main/java/com/example/retrofitunittest/MainActivity.java
+++ app/src/main/java/com/example/retrofitunittest/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.retrofitunittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        editText = findViewById(R.id.edittext);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.e(TAG, "beforeTextChanged() - charSequence : " + charSequence);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.e(TAG, "onTextChanged() - charSequence : " + charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.e(TAG, "afterTextChanged() - editable : " + editable.toString());
+            }
+        });
     }
 }
--- app/src/test/java/com/example/retrofitunittest/MovieApiTest.java
+++ app/src/test/java/com/example/retrofitunittest/MovieApiTest.java
@@ -4,12 +4,22 @@ import com.example.retrofitunittest.model.Movie;
 import com.example.retrofitunittest.model.Reviews;
 import com.example.retrofitunittest.model.Videos;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import retrofit2.Call;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -18,18 +28,15 @@ public class MovieApiTest extends BaseTestCase {
 
     @Test
     public void getTopRatedMovies() throws IOException {
-
         Call<Movie> topRatedList = getTmdbApiClient().movieInterface().getTopRatedMovies();
         Response<Movie> movieResponse = topRatedList.execute();
 
         assertEquals(movieResponse.code(), 200);
         assertTrue(movieResponse.isSuccessful());
-
     }
 
     @Test
     public void getPopularMovies() throws IOException {
-
         Call<Movie> popularMovies = getTmdbApiClient().movieInterface().getPopularMovies();
         Response<Movie> movieResponse = popularMovies.execute();
 
@@ -39,7 +46,6 @@ public class MovieApiTest extends BaseTestCase {
 
     @Test
     public void getMovieReviews() throws IOException {
-
         Call<Reviews> popularMovies = getTmdbApiClient().movieInterface().getMovieReviews(TestData.MOVIE_ID);
         Response<Reviews> reviewsResponse = popularMovies.execute();
 
@@ -49,12 +55,42 @@ public class MovieApiTest extends BaseTestCase {
 
     @Test
     public void getPopularVideos() throws IOException {
-
         Call<Videos> popularMovies = getTmdbApiClient().movieInterface().getMovieVideos(TestData.MOVIE_ID);
         Response<Videos> videosResponse = popularMovies.execute();
 
         assertEquals(videosResponse.code(), 200);
         assertTrue(videosResponse.isSuccessful());
+    }
+
+    /* https://answer-id.com/ko/63308629 */
+    // Set a response for retrofit to handle. You can copy a sample response from your server to simulate a correct result or an error.
+    // 처리할 레트로핏에 대한 응답을 설정합니다. 서버에서 샘플 응답을 복사하여 올바른 결과 또는 오류를 시뮬레이션할 수 있습니다.
+    // MockResponse can also be customized with different parameters to match your test needs
+    // MockResponse는 테스트 요구 사항에 맞게 다양한 매개변수로 사용자 정의할 수도 있습니다.
+    @Test
+    public void test() throws Exception {
+        MockWebServer server = new MockWebServer();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(server.url("http://api.themoviedb.org/3/movie/").toString())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+//        server.enqueue(new MockResponse().setBody());
+
+        MovieInterface movieInterface = retrofit.create(MovieInterface.class);
+
+        // With your service created you can now call its method that should  consume the MockResponse above
+        // 서비스가 생성되면 이제 위의 MockResponse를 사용해야 하는 메서드를 호출할 수 있습니다.
+        // You can then use the desired assertion to check if the result is as expected. For example:
+        // 그런 다음 원하는 어설션을 사용하여 결과가 예상대로인지 확인할 수 있습니다. 예를 들어:
+        Call<Movie> call = movieInterface.getPopularMovies();
+//        assertTrue(call.execute() != null);
+
+        Response<Movie> response = call.execute();
+        assertEquals(true, response.body().getResults() != null);
+//        assertEquals(response.code(), 401);
+//        assertTrue(response.isSuccessful());
+//        assertEquals(true, call.execute());
+        server.shutdown();
     }
 
 }
